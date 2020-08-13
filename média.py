166effca05824982a5a9e2e1b288f9917d7088b1
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-08-12 23:01:41 -0300

    Add files via upload

---
+++ média.py
@@ -0,0 +1,8 @@
+#Entrada de dados
+a = int(input('Digite o primeiro número: '))
+b = int(input('Digite o segundo número: '))
+c = int(input('Digite o terceiro número: '))
+#Processamento
+resultado= (a + b + c) // 2
+#Saída de dados
+print(f'O resultado da média anterior é {resultado}')
